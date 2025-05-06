9c03c753634072c658309b364c0d4e12edb3e8d2
gleidsonteixeira <gleidsondeveloper@gmail.com>
2025-05-06 17:05:51 -0300

    updates

--- clientes.js
+++ clientes.js
@@ -1,26 +1,9 @@
-let clientes = [
-    {
-        nome: "Conrado",
-        email: "conrado@gmail.com",
-        telefone: "(85) 00000-0000",
-        data: "05/05/2025"
-    },
-    {
-        nome: "Emanuel",
-        email: "emanuel@gmail.com",
-        telefone: "(85) 00000-0000",
-        data: "05/05/2025"
-    },
-    {
-        nome: "Ana",
-        email: "ana@gmail.com",
-        telefone: "(85) 00000-0000",
-        data: "05/05/2025"
-    }
-];
+let clientes = sessionStorage.getItem("clientes") ? JSON.parse(sessionStorage.getItem("clientes")) : [];
+
 
 function carregarClientes(listaDeClientes) {
     let tbodyElement = document.querySelector("#tabela");
+    tbodyElement.innerHTML = '';
     listaDeClientes.map((cliente) => {
         tbodyElement.innerHTML += `
             <tr class="*:leading-[40px]">
@@ -37,4 +20,18 @@ function carregarClientes(listaDeClientes) {
     })
 }
 
-carregarClientes(clientes)
+carregarClientes(clientes);
+
+function cadastrarCliente(form){
+    event.preventDefault();
+
+    // vão pegar os valores dos inputs do formulario e transformar em um objeto
+    let formData = new FormData(form);
+    let cliente = Object.fromEntries(formData.entries());
+    
+    // inserir o novo cliente no final do arrray clientes
+    clientes.push(cliente);
+    sessionStorage.setItem("clientes", JSON.stringify(clientes));
+    mostrarOverlay();
+    carregarClientes(clientes);
+}
